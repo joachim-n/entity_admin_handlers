73f790e89c0c725f40fd101b3da7c4f31a061052
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2019-03-30 21:34:35 +0000

    Fixed single bundle field UI route controller having stray hardcoded entity type label.

--- src/SingleBundleEntity/SingleBundleAdminController.php
+++ src/SingleBundleEntity/SingleBundleAdminController.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\entity_admin_handlers\SingleBundleEntity;
 
+use Drupal\Core\Routing\RouteMatchInterface;
+
 /**
  * Controller for the field UI base route.
  */
@@ -10,9 +12,14 @@ class SingleBundleAdminController {
   /**
    * Callback for the field UI base route.
    */
-  public function content() {
+  public function content(RouteMatchInterface $route_match) {
+    $entity_type_id = $route_match->getRouteObject()->getDefault('entity_type_id');
+    $entity_type = \Drupal::service('entity_type.manager')->getDefinition($entity_type_id);
+
     return [
-      '#markup' => t('Accounting client entity settings.'),
+      '#markup' => t('@label entity settings.', [
+        '@label' => $entity_type->getLabel(),
+      ]),
     ];
   }
 
